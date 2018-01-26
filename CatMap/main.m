c6abccd5eadca9c89098bfc6560d01ec652af908
jos3barros <barros_j@yahoo.com>
2018-01-26 21:33:35 -0200

    Initial Commit

---
+++ CatMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CatMap
+//
+//  Created by Jose on 26/1/18.
+//  Copyright © 2018 Jose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
